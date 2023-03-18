d018efcdbb807b8a4081d12e582a8951709ca331
Alex Kuznetsov <alex.y.kuznetsov@gmail.com>
2023-03-18 18:27:49 +1100

    form functionality without validation

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 const form = document.querySelector('#contactForm');
 
-function formLogic() {
+function formLogic(evt) {
+    evt.preventDefault();
     
+    const personalizedMessage = document.querySelector('#personalizedMessage');
     const firstName = form.querySelector('#firstName').value;
     const secondName = form.querySelector('#secondName').value;
     const email = form.querySelector('#email').value;
@@ -14,7 +16,26 @@ function formLogic() {
         phoneNumber
     }
 
-    console.log(detailsObject);
+    function getContactMethod() {
+        let contactMethod;
+        if (detailsObject.email.length && !detailsObject.phoneNumber) {
+            contactMethod = `email (${detailsObject.email})`
+        }
+        if (detailsObject.phoneNumber && !detailsObject.email) {
+            contactMethod = `phone (${detailsObject.phoneNumber})`
+        }
+        if (detailsObject.phoneNumber && detailsObject.email) {
+            contactMethod = `email (${detailsObject.email}) or phone (${detailsObject.phoneNumber})`
+        }
+        return contactMethod;
+    };
+    
+
+    const message = `Thank you for your submission, ${detailsObject.firstName}. 
+                    Our manager will contact you shortly via ${getContactMethod()}`
+    personalizedMessage.innerHTML = message;
+    form.classList.add('hidden');
+    personalizedMessage.classList.remove('hidden');
 }
 
 const submitButton = form.querySelector('#submitForm');
